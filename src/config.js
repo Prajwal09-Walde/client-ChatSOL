06adbeb16ff2207dca9adc30f8e7aa7412e9a67f
Prajwal09-Walde <pbwalde98@gmail.com>
2026-04-28 12:41:03 +0530

    env changed

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'https://server-chat-ac5t9073o-prajwal09-walde.vercel.app';
+export const API_URL = import.meta.env.VITE_API_URL || 'https://server-chat-sol-ufsg-git-main-prajwal09waldes-projects.vercel.app';
